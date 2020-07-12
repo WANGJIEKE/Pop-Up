895ad83ec4668b3460ae53bb9ca58eb4f8b6c4b7
WANGJIEKE <tjwang0221@gmail.com>
2020-07-11 17:37:09 -0700

    basic mechanism

--- main.cpp
+++ main.cpp
@@ -1,72 +1,96 @@
 // main.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <atomic>
+#include <algorithm>
+#include <chrono>
 #include <functional>
+#include <future>
+#include <iostream>
+#include <random>
 #include <thread>
 #include <vector>
+
 #include "PopUp.h"
 
-class PopUp {
-private:
-    std::thread _thread;
-    bool _finished;
-    bool _gameFailedOnThisPopUp;
-    std::function<bool(int)> _shouldFailGame;
-
-public:
-    std::wstring text;
-    std::wstring caption;
+struct MessageBoxParam
+{
+    std::wstring text, caption;
     unsigned int type;
-
-    PopUp(
-        const std::wstring& theText,
-        const std::wstring& theCaption,
-        const unsigned int& theType,
-        const std::function<bool(int)>& shouldFailGame = [](int) { return false; }
-    ) :
-        text(theText),
-        caption(theCaption),
-        type(theType),
-        _thread([this]()
-            {
-                int ret = MessageBoxRandPos(NULL, text.c_str(), caption.c_str(), type);
-                _finished = true;
-                _gameFailedOnThisPopUp = _shouldFailGame(ret);
-            }
-        ),
-        _finished(false),
-        _gameFailedOnThisPopUp(false),
-        _shouldFailGame(shouldFailGame)
-    {}
-
-    const std::thread& thread() const { return _thread; }
-
-    const bool& finished() const { return _finished; }
-
-    const bool& gameShouldFail() const { return _gameFailedOnThisPopUp; }
 };
 
-void mainLoop(int tickRate)
+static std::vector<MessageBoxParam> msgBoxParams
 {
+    {L"Close Me", L"LOL", MB_OK | MB_ICONINFORMATION},
+    {L"You can't close me", L"!", MB_OK | MB_ICONINFORMATION},
+    {L"Hahahaha", L"Haha", MB_OK | MB_ICONINFORMATION},
+    {L"_(:3」∠)_", L"Oops!", MB_OK | MB_ICONERROR}
+};
 
+static std::vector<std::future<int>> futures;
+static int maxMsgBox;
+
+int MessageBoxWithRandomContent()
+{
+    std::random_device rand;
+    std::default_random_engine engine(rand());
+    std::uniform_int_distribution dist(0ULL, msgBoxParams.size() - 1);
+    const MessageBoxParam& param = msgBoxParams[dist(engine)];
+    return MessageBoxRandPos(NULL, param.text.c_str(), param.caption.c_str(), param.type);
+}
+
+void mainLoop()
+{
+    using namespace std::chrono;
+    using namespace std::chrono_literals;
+    constexpr int tickPerSecond = 25;
+    constexpr milliseconds tickLen(1000 / tickPerSecond);
+    constexpr milliseconds spawnInterval(1500);
+    milliseconds messageBoxSpawnStart = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+
+    while (true)
+    {
+        milliseconds tickStart = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+
+        if (futures.size() == maxMsgBox)
+        {
+            std::cout << "You lose because the maximum message box number has been reached" << std::endl;
+            return;
+        }
+
+        futures.erase(std::remove_if(futures.begin(), futures.end(), [](const std::future<int>& future) { return future.wait_for(0ms) == std::future_status::ready; }), futures.end());
+        if (futures.empty())
+        {
+            std::cout << "You win because you just close all message boxes before a new one is generated" << std::endl;
+            return;
+        }
+
+        if (duration_cast<milliseconds>(system_clock::now().time_since_epoch()) - messageBoxSpawnStart >= spawnInterval)
+        {
+            futures.push_back(
+                std::async(std::launch::async, []() { return MessageBoxWithRandomContent(); })
+            );
+            messageBoxSpawnStart = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+        }
+
+        std::this_thread::sleep_for(
+            milliseconds(
+                tickLen - ((duration_cast<milliseconds>(system_clock::now().time_since_epoch()) - tickStart) % tickLen)
+            )
+        );
+    }
 }
 
 int main()
 {
-    std::thread threads[10];
-    for (int i = 0; i < 10; ++i)
+    int initialMsgBox = 3;
+    maxMsgBox = 20;
+
+    for (int i = 0; i < initialMsgBox; ++i)
     {
-        threads[i] = std::thread(
-            [i](){
-                MessageBoxRandPos(NULL, TEXT("Close Me!"), TEXT("Oops!"), MB_YESNOCANCEL | MB_ICONQUESTION);
-            }
+        futures.push_back(
+            std::async(std::launch::async, []() { return MessageBoxWithRandomContent(); })
         );
     }
-
-    for (int i = 0; i < 10; ++i)
-    {
-        threads[i].join();
-    }
+    mainLoop();
     return 0;
 }
